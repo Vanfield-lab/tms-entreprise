17cec153f69bcee5bfcc29c1d14af172bfe37a8c
vanfield-lab <mr.boat2003@gmail.com>
2026-03-08 00:36:52 +0000

    minor bug fixed

--- src/modules/bookings/index.ts
+++ src/modules/bookings/index.ts
@@ -1,5 +1,4 @@
 export { default as BookingsTablePage } from "./pages/BookingsTable";
-export { default as CreateBookingPage } from "./pages/CreateBooking";
 export { default as CreateBookingV2Page } from "./pages/CreateBookingV2";
 export { default as MyBookingsPage } from "./pages/MyBookings";
 export { default as CloseTripsPage } from "./pages/CloseTrips";
